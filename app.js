c9a7fcee653f7a0a325ba584e1909b04d05b3471
Your Name <azamatabraev07@gmail.com>
2024-02-15 23:52:00 +0500

    env added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -5,7 +7,7 @@ const getLocalFaker = require("./locale");
 const generateError = require("./error");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
